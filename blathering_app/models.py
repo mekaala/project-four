435919c14707970563ac64f94890526bc46403a5
mekaala <mekaala@gmail.com>
2020-03-31 20:14:12 -0400

    modified models, made migrations

--- blathering_app/models.py
+++ blathering_app/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Fish(models.Model):
     name = models.CharField(max_length=255)
-    spawn_month = models.CharField(max_length=255)
+    spawn_month_north = models.CharField(max_length=255)
+    spawn_month_south = models.CharField(max_length=255)
     spawn_time = models.CharField(max_length=255)
     habitat = models.CharField(max_length=255)
-    spawn_point = models.CharField(max_length=255)
     shadow_size = models.CharField(max_length=255)
     sell_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)])
     photo_url = models.CharField(max_length=400)
@@ -18,10 +18,12 @@ class Fish(models.Model):
 class Bug(models.Model):
     name = models.CharField(max_length=255)
     family = models.CharField(max_length=255)
-    spawn_month = models.CharField(max_length=255)
+    spawn_month_north = models.CharField(max_length=255)
+    spawn_month_south = models.CharField(max_length=255)
     spawn_time = models.CharField(max_length=255)
-    habitat = models.CharField(max_length=255)
-    spawn_point = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    movement = models.CharField(max_length=255)
+    dangerous = models.CharField(max_length=255)
     sell_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)])
     photo_url = models.CharField(max_length=400)
 
