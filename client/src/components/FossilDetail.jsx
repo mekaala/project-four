d4bcfb4f5880320034fb82edee0551a8cf79a459
mekaala <mekaala@gmail.com>
2020-04-03 17:25:59 -0400

    displays fossils such that elements containing null values are hidden

--- client/src/components/FossilDetail.jsx
+++ client/src/components/FossilDetail.jsx
@@ -12,7 +12,7 @@ class FossilDetail extends Component {
         this.fetchFossil(fossilId)
     }
 
-    fetchFish = async (fossilId) => {
+    fetchFossil = async (fossilId) => {
         try {
             const fossilResponse = await axios.get(`/api/v1/fossils/${fossilId}/?format=json`)
             this.setState({
@@ -24,14 +24,42 @@ class FossilDetail extends Component {
         }
     }
 
+    hideMiddle() {
+        const middle = document.getElementsByClassName("middle");
+        if (this.state.fossil.mid_piece != null) {
+            return middle;
+        } else if (this.state.fossil.mid_piece = null) {
+            return middle.style.display ='none';
+        }
+    }
+
+    hideTail() {
+        const tail = document.getElementsByClassName("tail");
+        if (this.state.fossil.tail_piece != null) {
+            return tail;
+        } else if (this.state.fossil.tail_piece = null) {
+            return tail.style.display ='none';
+        }
+    }
+
     render() {
         return (
             <div>
                 <img src={ this.state.fossil.photo_url } alt={ this.state.fossil.name }/>
                 <h1>{ this.state.fossil.name }</h1>
                 <h3>{ this.state.fossil.head_piece } - { this.state.fossil.head_price } Bells</h3>
-                <h3>{ this.state.fossil.mid_piece } - { this.state.fossil.mid_price } Bells</h3>
-                <h3>{ this.state.fossil.tail_piece } - { this.state.fossil.tail_price } Bells</h3>
+                <div className="middle">
+                    { this.hideMiddle()
+                        ? <h3>{ this.state.fossil.mid_piece } - { this.state.fossil.mid_price } Bells</h3>
+                        : null
+                    }
+                </div>
+                <div className="tail">
+                    { this.hideTail()
+                        ? <h3>{ this.state.fossil.tail_piece } - { this.state.fossil.tail_price } Bells</h3>
+                        : null
+                    }
+                </div>
             </div>
         );
     }
