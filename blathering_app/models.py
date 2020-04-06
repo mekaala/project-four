4e81d89af719246c33dd58db4c167e032c41f9ab
mekaala <mekaala@gmail.com>
2020-04-06 14:49:31 -0400

    added more fossil piece inputs

--- blathering_app/models.py
+++ blathering_app/models.py
@@ -23,7 +23,6 @@ class Bug(models.Model):
     spawn_time = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     movement = models.CharField(max_length=255)
-    dangerous = models.CharField(max_length=255)
     sell_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)])
     photo_url = models.CharField(max_length=400)
 
@@ -32,12 +31,16 @@ class Bug(models.Model):
 
 class Fossil(models.Model):
     name = models.CharField(max_length=255)
-    head_piece = models.CharField(max_length=255)
-    head_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)])
-    mid_piece = models.CharField(max_length=255, blank=True, null=True)
-    mid_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)], blank=True, null=True)
-    tail_piece = models.CharField(max_length=255, blank=True, null=True)
-    tail_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)], blank=True, null=True)
+    piece_one = models.CharField(max_length=255)
+    piece_one_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)])
+    piece_two = models.CharField(max_length=255, blank=True, null=True)
+    piece_two_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)], blank=True, null=True)
+    piece_three = models.CharField(max_length=255, blank=True, null=True)
+    piece_three_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)], blank=True, null=True)
+    piece_four = models.CharField(max_length=255, blank=True, null=True)
+    piece_four_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)], blank=True, null=True)
+    piece_five = models.CharField(max_length=255, blank=True, null=True)
+    piece_five_price = models.PositiveIntegerField(validators=[MaxValueValidator(999999)], blank=True, null=True)
     photo_url = models.CharField(max_length=400)
 
     def __str__(self):
--- blathering_app/serializers.py
+++ blathering_app/serializers.py
@@ -11,9 +11,9 @@ class FishSerializer(serializers.ModelSerializer):
 class BugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bug
-        fields = ('id', 'name', 'family', 'spawn_month_north', 'spawn_month_south', 'spawn_time', 'location', 'movement', 'dangerous', 'sell_price', 'photo_url')
+        fields = ('id', 'name', 'family', 'spawn_month_north', 'spawn_month_south', 'spawn_time', 'location', 'movement', 'sell_price', 'photo_url')
 
 class FossilSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fossil
-        fields = ('id', 'name', 'head_piece', 'head_price', 'mid_piece', 'mid_price', 'tail_piece', 'tail_price', 'photo_url')
+        fields = ('id', 'name', 'piece_one', 'piece_one_price', 'piece_two', 'piece_two_price', 'piece_three', 'piece_three_price', 'piece_four', 'piece_four_price', 'piece_five', 'piece_five_price', 'photo_url')
